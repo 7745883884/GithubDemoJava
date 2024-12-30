e1de7dc0b77f1ee16dd63729166199e090200ddb
lokesh choudhari <lokeshchoudhari1@gmail.com>
2024-12-30 19:53:16 +0530

    first java program

---
+++ demo.java
@@ -0,0 +1,16 @@
+public class demo
+{
+
+    String name = "lokesh";
+    String surname = "choudhari";
+      public void userinfo()
+{
+ System.out.println(name +","+surname);
+}
+public static void main(String []args)
+{
+   
+   demo d = new demo();
+   d.userinfo();
+}
+}
